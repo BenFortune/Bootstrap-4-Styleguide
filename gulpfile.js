e3cfdc7b6c57f599b6234dfa03e67958cc2e79f5
Ben Fortune <ben.fortune@gmail.com>
2015-09-12 21:55:01 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+var input = './src/scss/*.scss';
+var output = './dist/css';
+
+gulp.task('sass', function () {
+  return gulp
+    // Find all `.scss` files from the `./src/scss` folder
+    .src(input)
+    // Run Sass on those files
+    .pipe(sass())
+    // Write the resulting CSS in the output folder
+    .pipe(gulp.dest(output));
+});
