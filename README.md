549873bd49ba62eab98f4f2b6b663ed16f752a4a
Ben Fortune <ben.fortune@gmail.com>
2015-12-03 20:18:44 -0600

    Misc updates
    - float label work
    - Initial slider work

---
+++ README.md
@@ -0,0 +1,7 @@
+#Boostrap 4 Styleguide
+
+---
+
+####TO DO
+- Get SVG Icons working
+- Clean up toggle styling
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,53 @@
 $(document).ready(function() {
-	// Some stuff goes here
+
+	// Initialize Tooltip
 	$(function () {
 		$('[data-toggle="tooltip"]').tooltip()
 	});
+
+	// BEGIN Float Label Work
+	var floatInput = $('.float-label input');
+	var onClass = "float-label-on";
+	var showClass = "float-label";
+
+	floatInput.on('focus', function(e) {
+		floatLabelHandler.call(e.target);
+	});
+	floatInput.on('blur', function(e) {
+		floatLabelHandler.call(e.target);
+	});
+	floatInput.on('keyup', function(e) {
+		floatLabelHandler.call(e.target);
+	});
+
+	function floatLabelHandler() {
+		if (!this.value || this.value === "") {
+			$(this).removeClass(showClass);
+			$(this).prev().removeClass(onClass);
+		} else {
+			$(this).addClass(showClass);
+			$(this).prev().addClass(onClass);
+		}
+	}
+	// END Float Label Work
+
+	(function diceToggle() {
+		console.log('default toggle status');
+		var diceToggle = $('.dice-toggle-control');
+		var diceToggleLabel = $('.dice-toggle-label');
+		diceToggle.addClass('on');
+
+		// Click event
+		diceToggle.on('click', function() {
+			console.log('toggle clicked');
+			if ($(this).hasClass('on')) {
+				$(this).removeClass('on').addClass('off');
+				diceToggleLabel.text('Off');
+			} else {
+				$(this).removeClass('off').addClass('on');
+				diceToggleLabel.text('On');
+			}
+		})
+	}());
+
 });
