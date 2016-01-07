9d1f3c6fcd8b012e8b15bc754f45dad77a1d2ad6
Ben Fortune <ben.fortune@gmail.com>
2016-01-06 19:48:33 -0600

    Gulp work
    Adding scripts task for concatenating js files
    Change constant names

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,38 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
-const scssLint = require('gulp-scss-lint');
+const gSass = require('gulp-sass');
+const gScssLint = require('gulp-scss-lint');
+const gConcat = require('gulp-concat');
 
 const input = './src/scss/**/*.scss';
 const output = './dist/css';
 
+const jsInput = './src/js/*.js';
+const jsOutput = './dist/js';
+
 gulp.task('sass', function () {
-	return gulp
-	.src(input)
-	.pipe(sass())
+	return gulp.src(input)
+	.pipe(gSass())
 	.pipe(gulp.dest(output));
 });
 
 gulp.task('scss-lint', function() {
-	return gulp
-	.src(input)
-	.pipe(scssLint({
+	return gulp.src(input)
+	.pipe(gScssLint({
 		'config': 'scss-lint.yml',
 	}));
 });
 
 gulp.task('styles', function() {
-	return gulp
-	.src(input)
-	.pipe(scssLint({
+	return gulp.src(input)
+	.pipe(gScssLint({
 		'config': 'scss-lint.yml',
 	}))
-	.pipe(sass())
+	.pipe(gSass())
 	.pipe(gulp.dest(output));
-})
+});
+
+gulp.task('scripts', function() {
+	return gulp.src(jsInput)
+	.pipe(gConcat('scripts.js'))
+	.pipe(gulp.dest(jsOutput));
+});
