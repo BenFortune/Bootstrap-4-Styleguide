0045068b8360030d2ef2ac5627a60d0607054efa
Ben Fortune <ben.fortune@gmail.com>
2016-01-16 21:51:01 -0600

    Gulp work
    - Adding auto prefixer task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,7 @@ import gulp from 'gulp';
 import gSass from 'gulp-sass';
 import gScssLint from 'gulp-scss-lint';
 import gConcat from 'gulp-concat';
+import gAutoPrefixer from 'gulp-autoprefixer';
 
 const sassPaths = {
 	src: './src/scss/**/*.scss',
@@ -13,6 +14,11 @@ const sassPaths = {
 const jsPaths = {
 	src: './src/js/*.js',
 	dest: './build/js'
+}
+
+const autoPrefxConfig = {
+	browsers: ['last 2 versions'],
+	cascade: false
 }
 
 gulp.task('sass', () => {
@@ -34,6 +40,7 @@ gulp.task('styles', () => {
 		'config': 'scss-lint.yml',
 	}))
 	.pipe(gSass())
+	.pipe(gAutoPrefixer(autoPrefxConfig))
 	.pipe(gulp.dest(sassPaths.dest));
 });
 
